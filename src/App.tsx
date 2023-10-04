dec6477e36d7ea0af2777c593e109cc6835b8399
jason <jason.aghedo@gmail.com>
2023-10-04 12:55:53 +0100

    Fixed Merge Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Services from "./pages/Services";
-import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
-    <>
       <Routes>
-        {/* created an empty home page for testing */}
         <Route path="/services" element={<Services />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/ContactUs" element={<ContactUs />} />
-      </Routes>{" "}
-    </>
+      </Routes>
   );
 }
 
