6f88fe84968f263ca77f6d05f202102a793ac3a1
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-10-05 23:37:33 +0100

    start work on loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Services from "./pages/Services";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
+import Loader from "./pages/Loader";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/services" element={<Services />} />
       <Route path="/about-us" element={<AboutUs />} />
       <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/" element={<Loader />} />
     </Routes>
   );
 }
---
+++ src/pages/Loader.tsx
@@ -0,0 +1,38 @@
+const LeftIcon = () => (
+  <svg
+    width="307"
+    height="309"
+    viewBox="0 0 307 309"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M42.0883 268.556C97.778 322.986 187.268 322.192 241.558 266.646C281.71 225.566 300.779 155.958 304.795 105.94C305.659 95.1841 306.642 84.519 306.505 73.9757C306.347 61.7221 306.027 21.6716 295.432 10.6388C284.853 -0.377061 243.711 -0.280266 231.533 0.12832C210.199 0.843594 189.069 3.28421 167.776 7.62581C123.018 16.7526 73.2488 35.3217 38.9095 70.4552C-13.7742 124.358 -13.1927 214.524 42.0883 268.556Z"
+      fill="url(#paint0_linear_1064_150)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_1064_150"
+        x1="16"
+        y1="154"
+        x2="284.5"
+        y2="154"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#FE8D7B" stop-opacity="0.8" />
+        <stop offset="1" stop-color="#FFCF6A" stop-opacity="0.8" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+const Loader = () => {
+  return (
+    <main>
+        <LeftIcon />
+    </main>
+  );
+};
+
+export default Loader;
