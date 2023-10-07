5c33c43f5cca4f947ef03d807b25e881339cf507
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-10-07 22:22:29 +0100

    commented out unneccessary parts of it

--- src/components/ContactUs/Map.tsx
+++ src/components/ContactUs/Map.tsx
@@ -10,13 +10,13 @@ const Map = ({ position = [51.505, -0.09] }: Props) => {
 		<div className="relative z-0 h-full">
 			<div className="w-full h-full">
 				<MapContainer
-					center={[51.505, -0.09]}
-					zoom={13}
-					scrollWheelZoom={false}
+					// center={[51.505, -0.09]}
+					// zoom={13}
+					// scrollWheelZoom={false}
 					style={{ height: "100%" }}
 				>
 					<TileLayer
-						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						// attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					/>
 					<Marker position={position}>
