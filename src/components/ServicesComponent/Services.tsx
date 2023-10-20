e259626e950050dfa8c4d94e2f62ef6bdf66887a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-10-20 01:29:28 +0100

    fix: font on services page

--- src/components/ServicesComponent/Services.tsx
+++ src/components/ServicesComponent/Services.tsx
@@ -6,7 +6,7 @@ const Services = () => {
 					<p className="border border-gray-500 px-5 py-3   rounded-3xl w-fit text-gray-500 lg:text-xl">Our Services</p>
 				</div>
 				<div className="">
-					<div className="text-[32px] md:text-[40px] lg:text-[56px] xl:text-[64px] max-w-[663px] font-poppins">
+					<div className="text-[32px] md:text-[40px] leading-[48px] lg:text-[56px] lg:leading-[62px] xl:text-[64px] xl:leading-[70px] max-w-[663px] font-poppins font-medium">
             We provide a wide range of services
         </div>
 					{/* <div className="lg:text-[50px] max-w-[1023px] font-poppins">You have access to the wide range of services we provide!</div> */}
